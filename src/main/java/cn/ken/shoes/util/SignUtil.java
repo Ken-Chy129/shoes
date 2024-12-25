b09f662b581677d62ff369e520a9beb69aba3df9
Ken-Chy129 <ken-chy129@qq.com>
2024-12-26 00:54:07 +0800

    优化签名工具

--- src/main/java/cn/ken/shoes/util/SignUtil.java
+++ src/main/java/cn/ken/shoes/util/SignUtil.java
@@ -1,5 +1,6 @@
 package cn.ken.shoes.util;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -9,11 +10,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * @Description: 签名器
- * @Param:
- * @return:
- * @Author: fengping
- * @Date: 2020/5/26
+ * 签名工具
  */
 @Slf4j
 public class SignUtil {
@@ -25,8 +22,7 @@ public class SignUtil {
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> {
-                            if (entry.getValue() instanceof List) {
-                                List<?> list = (List<?>) entry.getValue();
+                            if (entry.getValue() instanceof List<?> list) {
                                 return list.stream()
                                         .map(Object::toString)
                                         .collect(Collectors.joining(","));
@@ -41,13 +37,13 @@ public class SignUtil {
         // 按照参数名ASCII码从小到大排序（字典序）
         List<Map.Entry<String, String>> sortedEntries = filteredParams.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
-                .collect(Collectors.toList());
+                .toList();
 
         // 使用URL键值对格式拼接成字符串stringA
         StringBuilder queryStringBuilder = new StringBuilder();
         try {
             for (Map.Entry<String, String> entry : sortedEntries) {
-                if (queryStringBuilder.length() > 0) {
+                if (!queryStringBuilder.isEmpty()) {
                     queryStringBuilder.append("&");
                 }
                 queryStringBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8))
@@ -63,16 +59,33 @@ public class SignUtil {
 
     public static void main(String[] args) {
         // 示例数据
-        Map<String, Object> params = new LinkedHashMap<>();
-        params.put("b", "B");
-        params.put("a", "A");
-        params.put("c", "");
-        params.put("d", Arrays.asList("D1", "D2", "D3"));
-        params.put("e", null);
-        params.put("f", "F");
+        Map<String, Object> params = new HashMap<>();
+        params.put("app_key", "4d1715e032c44b709ef4954ef13e0950");
+        params.put("appoint_no", "A14343543654");
+        Map<String, Object> son1 = new LinkedHashMap<>();
+        son1.put("spu_id", 81293);
+        son1.put("sku_id", 487752589);
+        son1.put("bar_code", "487752589");
+        son1.put("article_number", "wucaishi");
+        son1.put("appoint_num", 10);
+        son1.put("brand_id", 10444);
+        son1.put("category_id", 46);
+        Map<String, Object> son2 = new LinkedHashMap<>();
+        son2.put("spu_id", 81293);
+        son2.put("sku_id", 487752589);
+        son2.put("bar_code", "487752589");
+        son2.put("article_number", "wucaishi");
+        son2.put("appoint_num", 10);
+        son2.put("brand_id", 10444);
+        son2.put("category_id", 46);
+        params.put("sku_list", Arrays.asList(JSON.toJSONString(son1), JSON.toJSONString(son2)));
 
+        params.put("timestamp", 1603354338917L);
         // 调用方法并打印结果
         System.out.println(mapToQueryString(params));
+
+        String s = "app_key=4d1715e032c44b709ef4954ef13e0950&appoint_no=A14343543654&sku_list=%7B%22spu_id%22%3A81293%2C%22sku_id%22%3A487752589%2C%22bar_code%22%3A%22487752589%22%2C%22article_number%22%3A%22wucaishi%22%2C%22appoint_num%22%3A10%2C%22brand_id%22%3A10444%2C%22category_id%22%3A46%7D%2C%7B%22spu_id%22%3A81293%2C%22sku_id%22%3A487752589%2C%22bar_code%22%3A%22487752589%22%2C%22article_number%22%3A%22wucaishi%22%2C%22appoint_num%22%3A10%2C%22brand_id%22%3A10444%2C%22category_id%22%3A46%7D&timestamp=1603353500369fb91e9e96f054166b567eec1b170ae2b";
+        System.out.println(stringToMD5UpperCase(s));
     }
 
 
