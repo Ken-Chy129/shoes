abe54bfc9d8958915a6fe44de641d6c40671021b
Ken-Chy129 <ken-chy129@qq.com>
2024-12-28 23:14:51 +0800

    查询尺码价格&引入http代理

--- src/main/java/cn/ken/shoes/common/KickScrewApiCOnstant.java
+++ src/main/java/cn/ken/shoes/common/KickScrewApiCOnstant.java
@@ -7,4 +7,6 @@ public class KickScrewApiConstant {
     public static final String SEARCH_ITEMS = "https://www.kickscrew.com/_next/data/nRD-h3UUIV49b75S4DlzE/en-US/search.json";
 
     public static final String BATCH_UPLOAD_ITEMS = "https://api.crewsupply.kickscrew.com/sapi/v2/stock/batch-update";
+
+    public static final String SEARCH_ITEM_SIZE_PRICE = "https://kickscrewshop.myshopify.com/api/2024-04/graphql.json";
 }
