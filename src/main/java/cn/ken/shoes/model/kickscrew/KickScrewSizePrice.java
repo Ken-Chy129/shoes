7becdc2bd016cf61e8a90f8d71da9400f2213e61
Ken-Chy129 <ken-chy129@qq.com>
2024-12-29 00:11:16 +0800

    解析尺码价格

---
+++ src/main/java/cn/ken/shoes/model/kickscrew/KickScrewSizePrice.java
@@ -0,0 +1,13 @@
+package cn.ken.shoes.model.kickscrew;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class KickScrewSizePrice {
+
+    private String title;
+
+    private Map<String, String> price;
+}
