6f12f8055cb20d1c270517a9eb162af5858839ed
Ken-Chy129 <ken-chy129@qq.com>
2024-12-31 20:40:37 +0800

    根据品牌查询并解析商品

--- src/main/java/cn/ken/shoes/common/KickScrewApiCOnstant.java
+++ src/main/java/cn/ken/shoes/common/KickScrewApiCOnstant.java
@@ -2,11 +2,23 @@ package cn.ken.shoes.common;
 
 public class KickScrewApiConstant {
 
+    /**
+     * 查询分类相关的信息
+     */
     public static final String CATEGORY = "https://7ccjsevco9-dsn.algolia.net/1/indexes/*/queries";
 
-    public static final String SEARCH_ITEMS = "https://www.kickscrew.com/_next/data/nRD-h3UUIV49b75S4DlzE/en-US/search.json";
+    /**
+     * 查询商品，可以通过品牌、类别进行查询（参数不同）
+     */
+    public static final String SEARCH_ITEMS = "https://www.kickscrew.com/_next/data/ncP0UtJDEbZJfVQ_9gyKX/en-US/search.json";
 
-    public static final String BATCH_UPLOAD_ITEMS = "https://api.crewsupply.kickscrew.com/sapi/v2/stock/batch-update";
-
+    /**
+     * 用于查询商品尺码+价格，实际上该接口可以做很多功能（直接前端拼接sql作为参数进行查询）
+     */
     public static final String SEARCH_ITEM_SIZE_PRICE = "https://kickscrewshop.myshopify.com/api/2024-04/graphql.json";
+
+    /**
+     * B端批量上传商品api
+     */
+    public static final String BATCH_UPLOAD_ITEMS = "https://api.crewsupply.kickscrew.com/sapi/v2/stock/batch-update";
 }
