b397a4d860adf298113d26cf4e43d939ecf4cc09
Ken-Chy129 <ken-chy129@qq.com>
2025-01-15 00:46:53 +0800

    分页条件查询商品

--- src/main/java/cn/ken/shoes/mapper/KickScrewItemMapper.java
+++ src/main/java/cn/ken/shoes/mapper/KickScrewItemMapper.java
@@ -1,6 +1,7 @@
 package cn.ken.shoes.mapper;
 
 import cn.ken.shoes.model.entity.KickScrewItemDO;
+import cn.ken.shoes.model.kickscrew.KickScrewItemRequest;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,6 +10,10 @@ import java.util.List;
 
 @Mapper
 public interface KickScrewItemMapper extends BaseMapper<KickScrewItemDO> {
+
+    Integer count(KickScrewItemRequest request);
+
+    List<KickScrewItemDO> selectModelNoByCondition(KickScrewItemRequest request);
 
     void insertIgnore(KickScrewItemDO kickScrewItemDO);
 
