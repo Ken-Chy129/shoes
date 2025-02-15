bf36c184ba17ff974eaeeb40c83146e2787c0f1d
Ken-Chy129 <ken-chy129@qq.com>
2025-02-16 01:33:56 +0800

    调整比价逻辑

--- src/main/java/cn/ken/shoes/mapper/PoisonPriceMapper.java
+++ src/main/java/cn/ken/shoes/mapper/PoisonPriceMapper.java
@@ -1,15 +1,19 @@
 package cn.ken.shoes.mapper;
 
-import cn.ken.shoes.model.entity.BrandDO;
 import cn.ken.shoes.model.entity.PoisonPriceDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Set;
 
 @Mapper
 public interface PoisonPriceMapper extends BaseMapper<PoisonPriceDO> {
+
+    Long count();
+
+    List<PoisonPriceDO> selectPage(@Param("startIndex") Long startIndex, @Param("pageSize") Integer pageSize);
 
     List<PoisonPriceDO> selectListByModelNos(Set<String> modelNos);
 }
