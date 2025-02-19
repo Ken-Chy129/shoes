beb1655245aa261a4bdb58134fa3005f5b5316de
Ken-Chy129 <ken-chy129@qq.com>
2025-02-20 00:45:21 +0800

    查询品牌爬取配置

--- console/src/services/shoes.ts
+++ console/src/services/shoes.ts
@@ -9,6 +9,7 @@ enum PRICE_API {
 enum SETTING_API {
     QUERY_PRICE_SETTING = '/api/setting/queryPriceSetting',
     UPDATE_PRICE_SETTING = '/api/setting/updatePriceSetting',
+    QUERY_BRAND_SETTING = '/api/setting/queryBrandSetting',
 }
 
 export {PRICE_API, SETTING_API}
