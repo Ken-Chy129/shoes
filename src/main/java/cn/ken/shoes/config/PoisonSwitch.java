4ae4123c5f1def5ca4b34cb3dd3a8c14a774c9a2
Ken-Chy129 <ken-chy129@qq.com>
2025-02-26 23:28:30 +0800

    开关控制使用哪个接口查询得物价格

--- src/main/java/cn/ken/shoes/config/PoisonSwitch.java
+++ src/main/java/cn/ken/shoes/config/PoisonSwitch.java
@@ -6,4 +6,9 @@ public class PoisonSwitch {
      * 得物价格类型
      */
     public static Integer POISON_PRICE_TYPE = 0;
+
+    /**
+     * 查询价格接口
+     */
+    public static Boolean USE_NEW_API = true;
 }
