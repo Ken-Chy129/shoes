9abe5099470529c1b93a99860ab47922df59e0f1
Ken-Chy129 <ken-chy129@qq.com>
2025-02-27 23:23:25 +0800

    新增404页面路由

--- console/src/pages/404.tsx
+++ console/src/pages/404.tsx
@@ -6,10 +6,10 @@ const NoFoundPage: React.FC = () => (
   <Result
     status="404"
     title="404"
-    subTitle={useIntl().formatMessage({ id: 'pages.404.subTitle' })}
+    subTitle={useIntl().formatMessage({ id: '页面不存在' })}
     extra={
       <Button type="primary" onClick={() => history.push('/')}>
-        {useIntl().formatMessage({ id: 'pages.404.buttonText' })}
+        {useIntl().formatMessage({ id: '回到首页' })}
       </Button>
     }
   />
