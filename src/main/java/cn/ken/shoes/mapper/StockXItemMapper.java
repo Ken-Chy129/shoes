f8acc3935c0e7dfd613993feac1e86ceccb5c812
Ken-Chy129 <ken-chy129@qq.com>
2025-03-16 01:33:42 +0800

    insert ignore

--- src/main/java/cn/ken/shoes/mapper/StockXItemMapper.java
+++ src/main/java/cn/ken/shoes/mapper/StockXItemMapper.java
@@ -4,10 +4,9 @@ import cn.ken.shoes.model.entity.StockXItemDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 @Mapper
 public interface StockXItemMapper extends BaseMapper<StockXItemDO> {
 
+    int insertIgnore(StockXItemDO stockXItemDO);
 
 }
