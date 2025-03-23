096ab1a715475e48f820ad08923504f912187490
Ken-Chy129 <ken-chy129@qq.com>
2025-03-23 17:46:23 +0800

    移除无用代码

--- src/main/java/cn/ken/shoes/common/PoisonApiConstant.java
+++ src/main/java/cn/ken/shoes/common/PoisonApiConstant.java
@@ -36,9 +36,4 @@ public class PoisonApiConstant {
      * 根据spu查询价格
      */
     public static final String PRICE_BY_SPU_V2 = "http://134.175.182.182:6699/api/dewu/redis/price_token?spuid={spuId}&token={token}";
-
-    /**
-     * 得物查价接口
-     */
-    public static final String PRICE_BY_SPU_V3 = "http://8.142.81.233:8999/api/v1/dewu/productPrice?spu_id={spuId}";
 }
