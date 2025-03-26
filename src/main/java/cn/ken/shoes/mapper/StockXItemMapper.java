f5bf701343793afb2ed94f911246349bcf340716
Ken-Chy129 <ken-chy129@qq.com>
2025-03-27 00:39:09 +0800

    刷新绿叉商品和价格

--- src/main/java/cn/ken/shoes/mapper/StockXItemMapper.java
+++ src/main/java/cn/ken/shoes/mapper/StockXItemMapper.java
@@ -4,9 +4,12 @@ import cn.ken.shoes.model.entity.StockXItemDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface StockXItemMapper extends BaseMapper<StockXItemDO> {
 
     int insertIgnore(StockXItemDO stockXItemDO);
 
+    List<String> selectAllProductIds();
 }
