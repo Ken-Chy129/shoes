274beb1b75928bb9aab873ec00f22961fab1e48a
Ken-Chy129 <ken-chy129@qq.com>
2025-03-29 22:13:37 +0800

    kc价格类型和排序类型配置接口

--- src/main/java/cn/ken/shoes/config/StockXSwitch.java
+++ src/main/java/cn/ken/shoes/config/StockXSwitch.java
@@ -1,8 +1,11 @@
 package cn.ken.shoes.config;
 
+import cn.ken.shoes.common.StockXPriceEnum;
 import cn.ken.shoes.common.StockXSortEnum;
 
 public class StockXSwitch {
 
     public static StockXSortEnum SORT_TYPE = StockXSortEnum.FEATURED;
+
+    public static StockXPriceEnum PRICE_TYPE = StockXPriceEnum.EARN_MORE;
 }
