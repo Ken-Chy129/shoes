2e60e17149c844b2457bab449b79224a1e9893a8
Ken-Chy129 <ken-chy129@qq.com>
2025-04-22 00:04:39 +0800

    定时任务指定线程池

--- src/main/java/cn/ken/shoes/ShoesApplication.java
+++ src/main/java/cn/ken/shoes/ShoesApplication.java
@@ -3,7 +3,10 @@ package cn.ken.shoes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,4 +17,13 @@ public class ShoesApplication {
         SpringApplication.run(ShoesApplication.class, args);
     }
 
+    /**
+     * 定时任务指定线程池（默认为单线程）
+     */
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        return taskScheduler;
+    }
 }
