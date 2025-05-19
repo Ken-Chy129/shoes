0b477489de609041563f5291d89eae0f0f8cf224
Ken-Chy129 <ken-chy129@qq.com>
2025-05-20 00:06:33 +0800

    页面优化

--- console/src/pages/index.tsx
+++ console/src/pages/index.tsx
@@ -124,26 +124,42 @@ const SettingPage = () => {
     }
 
     return <>
+        <Card title={"通用配置"}>
+            <Form form={kcForm}
+                  style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap"}}>
+                <div style={{display: "flex"}}>
+                    <Form.Item name="exchangeRate" label="汇率">
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item style={{marginLeft: 50}}>
+                        <Button type="primary" htmlType="submit" onClick={updateKcSetting}>
+                            修改
+                        </Button>
+                    </Form.Item>
+                </div>
+            </Form>
+        </Card>
+        <br/>
         <Card title={"得物配置"}>
             <Form form={poisonForm}
                   style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap"}}>
                 <div style={{display: "flex"}}>
-                    <Form.Item name="apiMode" label="查价模式">
-                        <Select
-                            style={{width: 160}}
-                            placeholder="请选择字段"
-                            allowClear
-                            optionFilterProp="label"
-                            options={
-                                [
-                                    {label: '实时查询', value: 0},
-                                    {label: '缓存查询', value: 1},
-                                    {label: '综合模式', value: 2}
-                                ]
-                            }
-                        />
-                    </Form.Item>
-                    <Form.Item name="maxPrice" label="最大价格限制" style={{marginLeft: 20}}>
+                    {/*<Form.Item name="apiMode" label="查价模式">*/}
+                    {/*    <Select*/}
+                    {/*        style={{width: 160}}*/}
+                    {/*        placeholder="请选择字段"*/}
+                    {/*        allowClear*/}
+                    {/*        optionFilterProp="label"*/}
+                    {/*        options={*/}
+                    {/*            [*/}
+                    {/*                {label: '实时查询', value: 0},*/}
+                    {/*                {label: '缓存查询', value: 1},*/}
+                    {/*                {label: '综合模式', value: 2}*/}
+                    {/*            ]*/}
+                    {/*        }*/}
+                    {/*    />*/}
+                    {/*</Form.Item>*/}
+                    <Form.Item name="maxPrice" label="最大价格限制">
                         <Input/>
                     </Form.Item>
                     <Form.Item style={{marginLeft: 50}}>
@@ -159,10 +175,7 @@ const SettingPage = () => {
             <Form form={kcForm}
                   style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap"}}>
                 <div style={{display: "flex"}}>
-                    <Form.Item name="exchangeRate" label="汇率">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item name="freight" label="运费" style={{marginLeft: 20}}>
+                    <Form.Item name="freight" label="运费">
                         <Input/>
                     </Form.Item>
                     <Form.Item name="minProfit" label="最小利润" style={{marginLeft: 20}}>
@@ -184,34 +197,34 @@ const SettingPage = () => {
         </Card>
         <br/>
         <Card title={"stockx配置"}>
-            <Form form={stockxForm}
-                  style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap"}}>
-                <div style={{display: "flex"}}>
-                    <Form.Item name="expireTime" label="令牌有效期">
-                        <Input disabled={true} />
-                    </Form.Item>
-                    <Form.Item style={{marginLeft: 50}}>
-                        <Button type="primary" htmlType="submit" onClick={authorize}>
-                            认证
-                        </Button>
-                    </Form.Item>
-                    <Form.Item style={{marginLeft: 50}}>
-                        <Button type="primary" htmlType="submit" onClick={initToken}>
-                            初始化令牌
-                        </Button>
-                    </Form.Item>
-                    <Form.Item style={{marginLeft: 50}}>
-                        <Button type="primary" htmlType="submit" onClick={refreshToken}>
-                            刷新令牌
-                        </Button>
-                    </Form.Item>
-                </div>
-            </Form>
+            {/*<Form form={stockxForm}*/}
+            {/*      style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap"}}>*/}
+            {/*    <div style={{display: "flex"}}>*/}
+            {/*        <Form.Item name="expireTime" label="令牌有效期">*/}
+            {/*            <Input disabled={true}/>*/}
+            {/*        </Form.Item>*/}
+            {/*        <Form.Item style={{marginLeft: 50}}>*/}
+            {/*            <Button type="primary" htmlType="submit" onClick={authorize}>*/}
+            {/*                认证*/}
+            {/*            </Button>*/}
+            {/*        </Form.Item>*/}
+            {/*        <Form.Item style={{marginLeft: 50}}>*/}
+            {/*            <Button type="primary" htmlType="submit" onClick={initToken}>*/}
+            {/*                初始化令牌*/}
+            {/*            </Button>*/}
+            {/*        </Form.Item>*/}
+            {/*        <Form.Item style={{marginLeft: 50}}>*/}
+            {/*            <Button type="primary" htmlType="submit" onClick={refreshToken}>*/}
+            {/*                刷新令牌*/}
+            {/*            </Button>*/}
+            {/*        </Form.Item>*/}
+            {/*    </div>*/}
+            {/*</Form>*/}
             <Form form={stockxForm}
                   style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap"}}>
                 <div style={{display: "flex"}}>
                     <Form.Item name="accessToken" label="令牌">
-                        <Input />
+                        <Input/>
                     </Form.Item>
                     <Form.Item style={{marginLeft: 50}}>
                         <Button type="primary" htmlType="submit" onClick={updateToken}>
@@ -265,22 +278,6 @@ const SettingPage = () => {
                     <Form.Item style={{marginLeft: 50}}>
                         <Button type="primary" htmlType="submit" onClick={updateStockxSetting}>
                             更新配置
-                        </Button>
-                    </Form.Item>
-                </div>
-            </Form>
-        </Card>
-        <br/>
-        <Card title={"接口配置"}>
-            <Form form={kcForm}
-                  style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap"}}>
-                <div style={{display: "flex"}}>
-                    <Form.Item name="exchangeRate" label="汇率">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item style={{marginLeft: 50}}>
-                        <Button type="primary" htmlType="submit" onClick={updateKcSetting}>
-                            修改
                         </Button>
                     </Form.Item>
                 </div>
