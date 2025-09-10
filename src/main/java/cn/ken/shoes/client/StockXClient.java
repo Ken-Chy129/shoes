9afb8ee465a9155903713d7a634ff2568231fa21
Ken-Chy129 <ken-chy129@qq.com>
2025-09-11 02:05:17 +0800

    更新绿叉订单接口

--- src/main/java/cn/ken/shoes/client/StockXClient.java
+++ src/main/java/cn/ken/shoes/client/StockXClient.java
@@ -122,13 +122,11 @@ public class StockXClient {
 
     public void extendItem(String chainId) {
         JSONObject body = new JSONObject();
-        body.put("operationName", "RequestSellerShippingExtension");
+        body.put("operationName", "ExtendShipDate");
         JSONObject variables = new JSONObject();
         body.put("variables", variables);
-        JSONObject input = new JSONObject();
-        input.put("chainId", chainId);
-        variables.put("input", input);
-        body.put("query", "mutation RequestSellerShippingExtension($input: SellerShippingExtensionRequestInput) {\n  requestSellerShippingExtension(input: $input) {\n    approved\n    shipByDateExtendedTo\n    __typename\n  }\n}");
+        variables.put("chainId", chainId);
+        body.put("query", "mutation ExtendShipDate($chainId: String) {\n  requestSellerShippingExtension(\n    input: {chainId: $chainId}\n  ) {\n    approved\n    shipByDateExtendedTo\n    __typename\n  }\n}");
         queryPro(body.toJSONString());
     }
 
@@ -584,7 +582,7 @@ public class StockXClient {
         requestJson.put("operationName", "ViewerAsks");
         requestJson.put("query", "query ViewerAsks($query: String, $after: String, $pageSize: Int, $currencyCode: CurrencyCode, $state: AsksGeneralState, $filters: AsksFiltersInput, $sort: AsksSortInput, $order: AscDescOrderInput) {\n  viewer {\n    asks(\n      query: $query\n      after: $after\n      first: $pageSize\n      currencyCode: $currencyCode\n      state: $state\n      filters: $filters\n      sort: $sort\n      order: $order\n    ) {\n      pageInfo {\n        endCursor\n        hasNextPage\n        totalCount\n        }\n      edges {\n        node {\n          ...AskAttributes\n          }\n        }\n      }\n    }\n}\n\nfragment AskAttributes on Ask {\n  id\n  shippingExtensionRequested\n  orderNumber\n}");
         JSONObject variables = new JSONObject();
-        variables.put("pageSize", 1000);
+        variables.put("pageSize", 500);
         variables.put("sort", "LISTED_AT");
         variables.put("order", "DESC");
         variables.put("skipFlexEligible", true);
