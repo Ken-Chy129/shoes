2dc209a9a08d7e0dbc8f80628052594643bea88b
chenyanheng <chenyanheng@bytedance.com>
2025-10-18 18:21:11 +0800

    绿叉查询导出页面

--- console/src/services/stockx.ts
+++ console/src/services/stockx.ts
@@ -7,4 +7,10 @@ enum STOCKX_API {
     SEARCH = '/api/stockx/searchItems',
 }
 
-export {ORDER_API, STOCKX_API}
+enum SEARCH_TASK_API {
+    CREATE = '/api/stockx/createSearchTask',      // 创建搜索任务
+    GET_TASKS = '/api/stockx/getSearchTasks',     // 查询任务列表
+    GET_TASK = '/api/stockx/searchTask',          // 查询单个任务详情
+}
+
+export {ORDER_API, STOCKX_API, SEARCH_TASK_API}
