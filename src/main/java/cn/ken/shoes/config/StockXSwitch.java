424f2b3a55a7c7e8492cb5e50481557650ab58d7
Ken-Chy129 <ken-chy129@qq.com>
2026-01-27 01:44:12 +0800

    feat: 增加绿叉任务配置

--- src/main/java/cn/ken/shoes/config/StockXSwitch.java
+++ src/main/java/cn/ken/shoes/config/StockXSwitch.java
@@ -11,6 +11,20 @@ public class StockXSwitch {
 
     public static StockXPriceEnum PRICE_TYPE = StockXPriceEnum.SELL_FASTER;
 
+    // ==================== 任务配置 ====================
+
+    // 压价线程数
+    public static int TASK_PRICE_DOWN_THREAD_COUNT = 1;
+
+    // 每分钟压价数
+    public static int TASK_PRICE_DOWN_PER_MINUTE = 60;
+
+    // 压价排序字段：CREATED_AT, ITEM_TITLE, BID_ASK_SPREAD, PRICE, LOWEST_ASK, HIGHEST_BID, SIZE, UPDATED_AT
+    public static String TASK_LISTING_SORT = "CREATED_AT";
+
+    // 压价排序顺序：ASC, DESC
+    public static String TASK_LISTING_ORDER = "DESC";
+
     /**
      * 保存配置到文件
      */
