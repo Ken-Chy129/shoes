ca30087cd2d1eade9caba8ecb36b6d6cebce3467
Ken-Chy129 <ken-chy129@qq.com>
2026-02-01 23:51:21 +0800

    feat: 优化限流器，支持分钟维度限流

--- src/main/java/cn/ken/shoes/util/LimiterHelper.java
+++ src/main/java/cn/ken/shoes/util/LimiterHelper.java
@@ -3,6 +3,10 @@ package cn.ken.shoes.util;
 
 import cn.ken.shoes.config.StockXSwitch;
 import com.google.common.util.concurrent.RateLimiter;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+
+import java.time.Duration;
 
 public class LimiterHelper {
 
@@ -14,9 +18,15 @@ public class LimiterHelper {
     private static final RateLimiter DUNK_PRICE_LIMITER = RateLimiter.create(1);
     private static final RateLimiter DUNK_SALES_LIMITER = RateLimiter.create(10);
 
-    // StockX压价限流器，支持动态配置
-    private static volatile RateLimiter STOCKX_PRICE_DOWN_LIMITER = RateLimiter.create(StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE / 60.0);
+    // StockX压价限流器，使用Bucket4j支持分钟级限流
+    private static volatile Bucket STOCKX_PRICE_DOWN_BUCKET = createBucket(StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE);
     private static volatile int lastPriceDownPerMinute = StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE;
+
+    private static Bucket createBucket(int permitsPerMinute) {
+        return Bucket.builder()
+                .addLimit(Bandwidth.simple(permitsPerMinute, Duration.ofMinutes(1)))
+                .build();
+    }
 
     public static void limitPoisonItem() {
         POISON_ITEM_LIMITER.acquire();
@@ -46,17 +56,22 @@ public class LimiterHelper {
 
     /**
      * StockX压价接口限流，支持动态配置
+     * 使用Bucket4j实现分钟级限流，支持突发请求
      */
     public static void limitStockxPriceDown() {
         // 检查配置是否变更，如果变更则重新创建限流器
         if (lastPriceDownPerMinute != StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE) {
             synchronized (LimiterHelper.class) {
                 if (lastPriceDownPerMinute != StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE) {
-                    STOCKX_PRICE_DOWN_LIMITER = RateLimiter.create(StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE / 60.0);
+                    STOCKX_PRICE_DOWN_BUCKET = createBucket(StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE);
                     lastPriceDownPerMinute = StockXSwitch.TASK_PRICE_DOWN_PER_MINUTE;
                 }
             }
         }
-        STOCKX_PRICE_DOWN_LIMITER.acquire();
+        try {
+            STOCKX_PRICE_DOWN_BUCKET.asBlocking().consume(1);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
